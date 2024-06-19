7fe335522f6ce71eeef902fc47055c342ee0fede
JohanOrtiz251 <Johanortiz251@gmail.com>
2024-06-19 15:58:18 -0500

    first commit 3

--- server/index.js
+++ server/index.js
@@ -68,6 +68,23 @@ app.post('/login', (req, res) => {
   });
 });
 
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'sql5.freesqldatabase.com',
+  user: 'sql5714336',
+  password: 'mCt3ensGVJ',
+  database: 'sql5714336',
+  port: 3306,
+});
+connection.connect((error) => {
+  if (!error) {
+    console.log('Conexión exitosa');
+  } else {
+    console.error('Conexión fallida:', error.message);
+  }
+});
+module.exports = connection;
+
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
