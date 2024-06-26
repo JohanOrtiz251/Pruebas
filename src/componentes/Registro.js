9b2718bd69a8e3a34e1786b3478b99061bbfb294
JohanOrtiz251 <Johanortiz251@gmail.com>
2024-06-26 14:38:26 -0500

    prueba 4

--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Swal from 'sweetalert2';
 import { Link } from 'react-router-dom';
+import { FiSun, FiMoon, FiEye, FiEyeOff } from 'react-icons/fi';
 
 const Registro = ({ darkMode, toggleDarkMode }) => {
+
+    let URL = process.env.REACT_APP_ENVIRONMENT
+
     const [formData, setFormData] = useState({
         nombres: '',
         apellidos: '',
@@ -12,7 +16,7 @@ const Registro = ({ darkMode, toggleDarkMode }) => {
         celular: ''
     });
 
-
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -35,13 +39,27 @@ const Registro = ({ darkMode, toggleDarkMode }) => {
         }
 
         try {
-            const response = await fetch('http://localhost:3001/registro', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(formData)
+
+            // Este es para conectarse localmente
+
+            /* const response = await fetch('http://localhost:3001/registro', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
             });
+ */
+            // Este es para conectarse remotamente
+            console.log(URL)
+            const response = await fetch(`${URL}/registro`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json","Accept": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
 
             const responseData = await response.json();
             if (response.ok) {
@@ -70,76 +88,144 @@ const Registro = ({ darkMode, toggleDarkMode }) => {
         }
     };
 
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleGoogleLogin = () => {
+        alert('Redirigiendo al inicio de sesión de Google...');
+    };
+
     return (
-        <section className="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center">
-            <div className="max-w-md w-full p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
-                <h2 className="text-2xl font-semibold text-center mb-6">Registro de Usuario</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-1 gap-4">
-                        <input
-                            type="text"
-                            name="nombres"
-                            value={formData.nombres}
-                            onChange={handleChange}
-                            placeholder="Nombres"
-                            className="p-3 border border-gray-300 rounded-md w-full"
-                            required
-                        />
-                        <input
-                            type="text"
-                            name="apellidos"
-                            value={formData.apellidos}
-                            onChange={handleChange}
-                            placeholder="Apellidos"
-                            className="p-3 border border-gray-300 rounded-md w-full"
-                            required
-                        />
-                        <input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            placeholder="Correo electrónico"
-                            className="p-3 border border-gray-300 rounded-md w-full"
-                            required
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            placeholder="Contraseña"
-                            className="p-3 border border-gray-300 rounded-md w-full"
-                            required
-                        />
-                        <input
-                            type="password"
-                            name="confirmPassword"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                            placeholder="Confirmar contraseña"
-                            className="p-3 border border-gray-300 rounded-md w-full"
-                            required
-                        />
-                        <input
-                            type="text"
-                            name="celular"
-                            value={formData.celular}
-                            onChange={handleChange}
-                            placeholder="Celular"
-                            className="p-3 border border-gray-300 rounded-md w-full"
-                        />
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                    Flowbite
+                </Link>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create an account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                            <div className="grid grid-cols-2 gap-4">
+                                <div>
+                                    <label htmlFor="nombres" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                                    <input 
+                                        type="text" 
+                                        name="nombres" 
+                                        id="nombres" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                        placeholder="First Name" 
+                                        value={formData.nombres}
+                                        onChange={handleChange}
+                                        required 
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="apellidos" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                                    <input 
+                                        type="text" 
+                                        name="apellidos" 
+                                        id="apellidos" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                        placeholder="Last Name" 
+                                        value={formData.apellidos}
+                                        onChange={handleChange}
+                                        required 
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input 
+                                    type="email" 
+                                    name="email" 
+                                    id="email" 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                    placeholder="name@company.com" 
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    required 
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <div className="relative">
+                                    <input 
+                                        type={showPassword ? "text" : "password"} 
+                                        name="password" 
+                                        id="password" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                        placeholder="••••••••" 
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                        required 
+                                    />
+                                    <button 
+                                        type="button" 
+                                        className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none"
+                                        onClick={togglePasswordVisibility}
+                                    >
+                                        {showPassword ? <FiEyeOff className="w-5 h-5 text-gray-500" /> : <FiEye className="w-5 h-5 text-gray-500" />}
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                <input 
+                                    type="password" 
+                                    name="confirmPassword" 
+                                    id="confirm-password" 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                    placeholder="••••••••" 
+                                    value={formData.confirmPassword}
+                                    onChange={handleChange}
+                                    required 
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="celular" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+                                <input 
+                                    type="text" 
+                                    name="celular" 
+                                    id="celular" 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                    placeholder="Phone Number" 
+                                    value={formData.celular}
+                                    onChange={handleChange}
+                                    required 
+                                />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#!">Terms and Conditions</a></label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                            <button onClick={handleGoogleLogin} className="w-full bg-red-600 text-white hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-3">Sign up with Google</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400 mt-3">
+                                Already have an account? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                            </p>
+                        </form>
                     </div>
-                    <button
-                        type="submit"
-                        className="mt-6 bg-blue-500 hover:bg-blue-600 text-white font-semibold p-3 rounded-md w-full"
-                    >
-                        Registrar
-                    </button>
-                </form>
-                <p className="mt-4 text-center">
-                    ¿Ya tienes una cuenta? <Link to="/login" className="text-blue-500 hover:underline">Inicia sesión aquí</Link>
-                </p>
+                </div>
+                <Link to="/" className="absolute top-4 left-4 text-gray-800 dark:text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                    </svg>
+                </Link>
+                <button onClick={toggleDarkMode} className="absolute top-4 right-4 flex items-center text-gray-800 dark:text-white focus:outline-none">
+                    {darkMode ? (
+                        <FiSun className="w-5 h-5 mr-1" />
+                    ) : (
+                        <FiMoon className="w-5 h-5 mr-1" />
+                    )}
+                </button>
             </div>
         </section>
     );
